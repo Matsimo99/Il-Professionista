514644a46db93c77f3392937f7aff5a414352522
Matsimo99 <coppolam461@gmail.com>
2025-02-25 09:25:13 +0100

    Revert "aggiornamento"
    This reverts commit 4de84e461e37f0933ebd29e01762079f35c7d410.

--- index.php
+++ index.php
@@ -43,23 +43,7 @@ if (!$result_professionisti) {
             <img src="logo1.jpg" alt="Logo Azienda" width="150" height="150">
         </div>
         <nav>
-            <!-- Icona hamburger per dispositivi mobili -->
-            <div class="hamburger" id="hamburger-icon">
-                <div></div>
-                <div></div>
-                <div></div>
-            </div>
-
-            <!-- Menu di navigazione -->
             <ul class="navbar-links">
-                <li><a href="index.php">Home</a></li>
-                <li><a href="chi_siamo.php">Chi Siamo</a></li>
-                <li><a href="trattamento_dati.php">Trattamento dei Dati</a></li>
-                <li><a href="lavora_con_noi.php">Lavora con noi</a></li>
-            </ul>
-
-            <!-- Menu a tendina per dispositivi mobili -->
-            <ul class="navbar-links-mobile" id="navbar-mobile">
                 <li><a href="index.php">Home</a></li>
                 <li><a href="chi_siamo.php">Chi Siamo</a></li>
                 <li><a href="trattamento_dati.php">Trattamento dei Dati</a></li>
@@ -67,16 +51,6 @@ if (!$result_professionisti) {
             </ul>
         </nav>
     </header>
-
-    <script>
-        // Funzione per mostrare/nascondere il menu a tendina
-        const hamburgerIcon = document.getElementById("hamburger-icon");
-        const navbarMobile = document.getElementById("navbar-mobile");
-
-        hamburgerIcon.addEventListener("click", () => {
-            navbarMobile.classList.toggle("active");
-        });
-    </script>
 
     <!-- Sezione di ricerca orizzontale -->
     <section class="search-section">
